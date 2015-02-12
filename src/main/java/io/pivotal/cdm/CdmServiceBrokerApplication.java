3ab9e71ddded5b7de8bc339af88b6bede99c676a
krujos <jkruck@pivotal.io>
2015-02-12 14:46:44 -0800

    spring-starter-initial

---
+++ src/main/java/io/pivotal/cdm/CdmServiceBrokerApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdmServiceBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdmServiceBrokerApplication.class, args);
+    }
+}
