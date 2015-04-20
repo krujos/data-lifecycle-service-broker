9d38e459ee5e462e00c379a31f2ea91ad4676ff3
krujos <jkruck@pivotal.io>
2015-04-20 10:45:00 -0500

    Impl for postgres tests

--- src/main/java/io/pivotal/cdm/postgres/PostgresDataProvider.java
+++ src/main/java/io/pivotal/cdm/postgres/PostgresDataProvider.java
@@ -2,6 +2,9 @@ package io.pivotal.cdm.postgres;
 
 import io.pivotal.cdm.provider.DataProvider;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -10,6 +13,17 @@ import java.util.Map;
 public class PostgresDataProvider implements DataProvider {
     @Override
     public void sanitize(String script, Map<String, Object> creds) {
+        //We assume that the URI has username, password and db embedded in it.
+        checkForURI(creds);
+
+        try {
+            Connection connection = DriverManager.getConnection((String) creds.get("url"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkForURI(Map<String, Object> creds) {
         if ( !creds.containsKey("uri")) {
             throw new IllegalArgumentException("Credentials lack required " +
                     "`uri` parameter");
--- src/test/java/io/pivotal/cdm/postgres/PostgresDataProviderTest.java
+++ src/test/java/io/pivotal/cdm/postgres/PostgresDataProviderTest.java
@@ -1,19 +1,41 @@
 package io.pivotal.cdm.postgres;
 
-import org.junit.Test;
+import static org.Mockito.*;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PostgresDataProviderTest {
 
+
+    @InjectMocks
+    DriverManager driveManager;
+
+    PostgresDataProvider dataProvider;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        dataProvider = new PostgresDataProvider();
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowIllegalArgumentForMissingUri() {
-        PostgresDataProvider dataProvider = new PostgresDataProvider();
         Map<String, Object> creds = new HashMap<>();
         creds.put("username", "username");
         creds.put("password", "password");
         //No URI!
         dataProvider.sanitize("", creds);
+    }
+
+    @Test
+    public void itShouldGetADriverConnectionWithUserAndPass() {
+        when(DriverManager.getConnection())
     }
 }
